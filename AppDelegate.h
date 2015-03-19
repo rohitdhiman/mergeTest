642e10f050af784f2c1d1c98ac348c05faf01e53
Rohit Dhiman <sp.rohit385@gmail.com>
2015-03-19 16:46:57 +0200

    Appdelegate added

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PieChartDemo
+//
+//  Created by Rohit Dhiman on 16/03/15.
+//  Copyright (c) 2015 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PieChartViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, strong) PieChartViewController *pieChartViewController;
+
+@end
